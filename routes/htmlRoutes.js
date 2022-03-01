293e5296e89e367de1a5e8c205fc8dd77eecb208
ebeltz <ebbeltz@gmail.com>
2022-02-28 20:49:09 -0500

    added route to pull saved notes to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,8 +5,12 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
+router.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../db/db.json"));
+});
+
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
